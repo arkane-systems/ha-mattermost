bb7249940a9687453657914cdcad7bda0700de4e
Alistair Young <avatar@arkane-systems.net>
2024-11-26 17:29:56 -0600

    Experimental config and init.

--- custom_components/mattermost/__init__.py
+++ custom_components/mattermost/__init__.py
@@ -2,34 +2,61 @@
 
 from __future__ import annotations
 
+import logging
+
+from homeassistant.auth.providers.command_line import CONFIG_SCHEMA
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import Platform, CONF_NAME
 from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv
+from homeassistant.helpers.typing import ConfigType
 
-# TODO List the platforms that you want to support.
-# For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = [Platform.LIGHT]
+from .const import (
+    DOMAIN,
 
-# TODO Create ConfigEntry type alias with API object
-# TODO Rename type alias and update all entry annotations
-type New_NameConfigEntry = ConfigEntry[MyApi]  # noqa: F821
+    DATA_HASS_CONFIG
+)
 
+_LOGGER = logging.getLogger(__name__)
 
-# TODO Update entry annotation
-async def async_setup_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
-    """Set up Mattermost from a config entry."""
+# Platforms which this integration supports.
+PLATFORMS: list[Platform] = [Platform.NOTIFY]
 
-    # TODO 1. Create API instance
-    # TODO 2. Validate the API connection (and authentication)
-    # TODO 3. Store an API object for your platforms to access
-    # entry.runtime_data = MyAPI(...)
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
 
-    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up the Mattermost integration."""
+    _LOGGER.info("Mattermost integration started.")
 
+    hass.data[DATA_HASS_CONFIG] = config
     return True
 
+async def async_setup_entry (hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up a Mattermost instance from a config entry."""
+    _LOGGER.info("Set up Mattermost instance " + entry.data[CONF_NAME])
 
-# TODO Update entry annotation
-async def async_unload_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
-    """Unload a config entry."""
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    return True
+
+async def async_unload_entry (hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Shut down and unload a Mattermost instance and its config entry."""
+
     return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+
+# # TODO Create ConfigEntry type alias with API object
+# # TODO Rename type alias and update all entry annotations
+# type New_NameConfigEntry = ConfigEntry[MyApi]  # noqa: F821
+#
+#
+# # TODO Update entry annotation
+# async def async_setup_entry(hass: HomeAssistant, entry: New_NameConfigEntry) -> bool:
+#     """Set up Mattermost from a config entry."""
+#
+#     # TODO 1. Create API instance
+#     # TODO 2. Validate the API connection (and authentication)
+#     # TODO 3. Store an API object for your platforms to access
+#     # entry.runtime_data = MyAPI(...)
+#
+#     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+#
+#     return True
--- custom_components/mattermost/const.py
+++ custom_components/mattermost/const.py
@@ -1,3 +1,5 @@
 """Constants for the Mattermost integration."""
 
 DOMAIN = "mattermost"
+
+DATA_HASS_CONFIG = "mattermost_hass_config"
