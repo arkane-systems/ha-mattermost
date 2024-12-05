5af9e13531abce1050b6e351d6e04fb13129fc6f
Alistair Young <avatar@arkane-systems.net>
2024-12-05 09:55:27 -0600

    Attempt actual login.

--- custom_components/mattermost/config_flow.py
+++ custom_components/mattermost/config_flow.py
@@ -5,9 +5,11 @@ from __future__ import annotations
 import logging
 from typing import Any
 
+import mattermost
 import voluptuous as vol
 from homeassistant.config_entries import ConfigFlow, ConfigFlowResult
 from homeassistant.const import CONF_ACCESS_TOKEN, CONF_FRIENDLY_NAME, CONF_URL
+from homeassistant.exceptions import HomeAssistantError
 
 from .const import DOMAIN, CONF_CHANNEL
 
@@ -35,69 +37,33 @@ class MattermostFlowHandler(ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             try:
                 info = user_input
+
+                await self.test_credentials (info)
+
+            except LoginFailed as exception:
+                _LOGGER.error(exception)
+                errors["base"] = "auth"
             except Exception:
                 _LOGGER.exception("Unexpected exception.")
                 errors["base"] = "unknown"
             else:
-                return self.async_create_entry(title=info["friendly_name"], data=user_input)
+                return self.async_create_entry(title=info[CONF_FRIENDLY_NAME], data=user_input)
 
         return self.async_show_form(
             step_id="user", data_schema=CONFIG_SCHEMA, errors=errors
         )
 
+    async def test_credentials (self, info: dict[str, Any]) -> None:
+        """Validate provided credentials."""
 
-# class PlaceholderHub:
-#     """Placeholder class to make tests pass.
-#
-#     TODO Remove this placeholder class and replace with things from your PyPI package.
-#     """
-#
-#     def __init__(self, host: str) -> None:
-#         """Initialize."""
-#
-#     async def authenticate(self, username: str, password: str) -> bool:
-#         """Test if we can authenticate with the host."""
-#
-#
-# async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str, Any]:
-#     """Validate the user input allows us to connect.
-#
-#     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
-#     """
-#     # TODO validate the data can be used to set up a connection.
-#
-#     # If your PyPI package is not built with async, pass your methods
-#     # to the executor:
-#     # await hass.async_add_executor_job(
-#
-#
-#     if not await hub.authenticate(data[CONF_USERNAME], data[CONF_PASSWORD]):
-#         raise InvalidAuth
-#
-#     # If you cannot connect:
-#     # throw CannotConnect
-#     # If the authentication is wrong:
-#     # InvalidAuth
-#
-#     # Return info that you want to store in the config entry.
-#
-#
-# class ConfigFlow(ConfigFlow, domain=DOMAIN):
-#     """Handle a config flow for Mattermost."""
-#
-#
-#     async def async_step_user(
-#         self, user_input: dict[str, Any] | None = None
-#     ) -> ConfigFlowResult:
-#         """Handle the initial step."""
-#         if user_input is not None:
-#
-#         return self.async_show_form(
-#
-#
-# class CannotConnect(HomeAssistantError):
-#     """Error to indicate we cannot connect."""
-#
-#
-# class InvalidAuth(HomeAssistantError):
-#     """Error to indicate there is invalid auth."""
+        # Create API instance
+        mm = mattermost.MMApi(info[CONF_URL])
+        uid = mm.login(bearer=info[CONF_ACCESS_TOKEN])
+
+        if uid is None:
+            raise LoginFailed
+
+        mm.logout()
+
+class LoginFailed(HomeAssistantError):
+    """Error to indicate that we cannot log in to Mattermost."""
